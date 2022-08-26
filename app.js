f5bd6e696494d0e6a5d6f378ee8ad73921cce746
SOO <66086544+aubepluieh3@users.noreply.github.com>
2022-08-26 22:40:25 +0900

    #5.Saving Username

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
-   event.preventDefault();
-   loginForm.classList.add(HIDDEN_CLASSNAME);
-   const username = loginInput.value;
-   greeting.innerText = `Hello ${username}`;
-   greeting.classList.remove(HIDDEN_CLASSNAME);
+   event.preventDefault(); // 브라우저 기본 동작 막기
+   loginForm.classList.add(HIDDEN_CLASSNAME); // form에 hidden class 추가
+   const username = loginInput.value; // loginInput의 값을 변수에 저장
+   localStorage.setItem("username", username);
+   greeting.innerText = `Hello ${username}`; // string안에 변수값 넣기
+   greeting.classList.remove(HIDDEN_CLASSNAME); // h1에서 hidden class 삭제
 }
 
 
