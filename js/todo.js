0ae663d8d62d66a0c9f7a92276674374fddd1768
SOO <66086544+aubepluieh3@users.noreply.github.com>
2022-08-27 21:10:16 +0900

    #13.Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span); //li는 span이라는 자식을 가지게 됨
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
