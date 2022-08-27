9f318d19eedcc0a9e5fe0e27c8359d740a99a0b3
SOO <66086544+aubepluieh3@users.noreply.github.com>
2022-08-27 13:50:38 +0900

    #8.Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,10 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date();
+    clock.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);
+getClock();//wevsite가 load 되자마자 실행
+setInterval(getClock, 1000);//매초마다 getClock 실행
     
