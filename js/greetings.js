8387e5161a133f488e1a5362da610365d7c09fe1
SOO <66086544+aubepluieh3@users.noreply.github.com>
2022-09-13 19:54:40 +0900

    #24. Add CheckTodoButton & CSS update

--- js/greetings.js
+++ js/greetings.js
@@ -6,23 +6,23 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-   event.preventDefault(); // 브라우저 기본 동작 막기
-   loginForm.classList.add(HIDDEN_CLASSNAME); // form에 hidden class 추가
-   const username = loginInput.value; // loginInput의 값을 변수에 저장
-   localStorage.setItem(USERNAME_KEY, username);
-   paintGreetings(username);
+  event.preventDefault(); // 브라우저 기본 동작 막기
+  loginForm.classList.add(HIDDEN_CLASSNAME); // form에 hidden class 추가
+  const username = loginInput.value; // loginInput의 값을 변수에 저장
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`; // string안에 변수값 넣기
-    greeting.classList.remove(HIDDEN_CLASSNAME); // h1에서 hidden class 삭제
+function paintGreetings(username) {
+  greeting.innerText = `Hi ${username}💛 `; // string안에 변수값 넣기
+  greeting.classList.remove(HIDDEN_CLASSNAME); // h1에서 hidden class 삭제
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY); 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 //유저 정보 유무 확인
-if(savedUsername === null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME);//show the form
-    loginForm.addEventListener("submit", onLoginSubmit);
-}else{
-    paintGreetings(savedUsername); // show the greetings
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME); //show the form
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername); // show the greetings
 }
--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,20 @@
-const API_KEY="0398f545428592ef181b248b7385b142";
+const API_KEY = "0398f545428592ef181b248b7385b142";
 
-function onGeoOk(position){
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-    .then(Response => Response.json())
-    .then(data=>{
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText=data.name;
-        weather.innerText=`${data.weather[0].main} / ${data.main.temp}`;
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
-function onGeoError(){
-    alert("Can't find you. No weather for you.")
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
